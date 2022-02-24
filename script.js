b04b2c0a19e3053046352ea5c25faa9da7edd093
supersonik12 <smf1238@outlook.com>
2022-02-24 11:18:09 -0800

    Displays current date and greeting

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// SET HOME PAGE TEXT BASED ON DATE AND TIME
+var d = new Date();
+
+// Sets current date in MM/DD/YYYY format
+document.getElementById("date").innerText = "Today's date is " + d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear();
+
+// Greeting based on current time
+var greeting = "";
+if (d.getHours() < 12) {
+    greeting = "Good morning!";
+} else if (d.getHours() < 17) {
+    greeting = "Good afternoon!";
+} else if (d.getHours() < 21) {
+    greeting = "Good evening!";
+} else {
+    greeting = "Good night!";
+}
+document.getElementById("greeting").innerText = greeting;
+
 function changeColor(color) {
     document.body.style.backgroundColor = color;
 }
