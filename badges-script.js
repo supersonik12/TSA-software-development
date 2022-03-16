1fcca607ce7da5350c8ccd71abb2351ec73afd5d
Rishitha Ravi <rishitharavi@Rishithas-MacBook-Pro.local>
2022-03-15 22:34:51 -0700

    fixed nav bar

--- badges-script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    if (document.getElementById("nav").style.display == "block") {
-        document.getElementById("nav").style.display = "none";
-    } else {
-        document.getElementById("nav").style.display = "block";
-    }
-}
